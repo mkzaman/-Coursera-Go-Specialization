50d07ddee93fc0e6666cfd90594db9e10f69d365
mkzaman <mkzamanalive@gmail.com>
2021-07-14 00:03:37 +0600

    findian commit

--- Getting Started With Go/Week 2/2/findian.go
+++ Getting Started With Go/Week 2/2/findian.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	var temp string
-	fmt.Scanln(&temp)
-	result := strings.Contains(temp, "a & i & n")
 
-	if result {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	temp := scanner.Text()
+	caseInsensitive := strings.ToLower(temp)
+	first := string(caseInsensitive[0])
+	last := string(caseInsensitive[len(caseInsensitive)-1:])
+
+	if strings.Compare(first, "i") == 0 && strings.Compare(last, "n") == 0 && strings.Contains(caseInsensitive, "a") {
 		fmt.Println("Found!")
 	} else {
 		fmt.Println("Not Found!")
